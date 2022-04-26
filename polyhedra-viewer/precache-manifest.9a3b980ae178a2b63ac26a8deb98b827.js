54770ed2c7715c264c336e31ea861566536407eb
Andy Baker <andy@andybak.net>
2022-04-26 12:17:09 +0100

    Add polyhedra-viewer

---
+++ polyhedra-viewer/precache-manifest.9a3b980ae178a2b63ac26a8deb98b827.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccf29d5f6a651e02a3d8154f35542e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd601838a2ce19dd08bc",
+    "url": "/static/css/6.5ff2ddb2.chunk.css"
+  },
+  {
+    "revision": "e361ef30920ce4e12564",
+    "url": "/static/css/main.09f423dd.chunk.css"
+  },
+  {
+    "revision": "51aff129fc149c555fb2",
+    "url": "/static/js/0.84ecc87f.chunk.js"
+  },
+  {
+    "revision": "6ed35c3c632609049c82",
+    "url": "/static/js/1.c000f970.chunk.js"
+  },
+  {
+    "revision": "9d86572ad7019145f981",
+    "url": "/static/js/4.5933d65c.chunk.js"
+  },
+  {
+    "revision": "86222fc8710fb786cb21",
+    "url": "/static/js/5.0fc1f4ae.chunk.js"
+  },
+  {
+    "revision": "6bc5e71b32cafdf29af7f0439889dbe9",
+    "url": "/static/js/5.0fc1f4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd601838a2ce19dd08bc",
+    "url": "/static/js/6.7de32f86.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/6.7de32f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5462cb4aba17f69159bd",
+    "url": "/static/js/7.503dffa2.chunk.js"
+  },
+  {
+    "revision": "799fc59a22455ae9787f160cb1809238",
+    "url": "/static/js/7.503dffa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998748d06431c1b2cf42",
+    "url": "/static/js/8.b7b22dac.chunk.js"
+  },
+  {
+    "revision": "e361ef30920ce4e12564",
+    "url": "/static/js/main.766c1f96.chunk.js"
+  },
+  {
+    "revision": "f544e74d37786e7174fe",
+    "url": "/static/js/runtime-main.8bd1b0d5.js"
+  },
+  {
+    "revision": "7909547f54a47e2f950b0aabc6042876",
+    "url": "/static/media/sad-scutoid.7909547f.png"
+  },
+  {
+    "revision": "a380e39a06d25296f507c68f0776c8e5",
+    "url": "/static/media/transitions.a380e39a.mp4"
+  }
+]);
